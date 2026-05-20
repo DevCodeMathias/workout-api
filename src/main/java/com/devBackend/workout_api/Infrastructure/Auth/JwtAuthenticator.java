ece781dd4a9ba4814818cb1060379c787931405e
DevCodeMathias <lauramathias03@gmail.com>
2026-05-20 20:33:58 -0300

    feat: add authenticated route to list all activities

--- src/main/java/com/devBackend/workout_api/Infrastructure/Auth/JwtAuthenticator.java
+++
@@ -1,9 +0,0 @@
-package com.devBackend.workout_api.Infrastructure.Auth;
-
-public interface JwtAuthenticator {
-    void authenticate(String authorizationHeader);
-
-    void authenticateEmployee(String authorizationHeader, String employeeId);
-
-    String getAuthenticatedEmployeeId(String authorizationHeader);
-}
