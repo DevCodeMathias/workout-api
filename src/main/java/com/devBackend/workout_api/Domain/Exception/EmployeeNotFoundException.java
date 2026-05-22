6625d25b14e96db57229709f17b6ae4ab0ff3b38
DevCodeMathias <lauramathias03@gmail.com>
2026-05-22 09:22:08 -0300

    refactor: EmployeeNotFoundException https status

--- src/main/java/com/devBackend/workout_api/Domain/Exception/EmployeeNotFoundException.java
+++ src/main/java/com/devBackend/workout_api/Domain/Exception/EmployeeNotFoundException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class EmployeeNotFoundException extends ApplicationException {
     public EmployeeNotFoundException(String id) {
-        super(HttpStatus.NOT_FOUND, "EMPLOYEE_NOT_FOUND", "Employee not found with id: " + id);
+        super(HttpStatus.UNAUTHORIZED, "EMPLOYEE_NOT_FOUND", "Employee not found with id: " + id);
     }
 }
